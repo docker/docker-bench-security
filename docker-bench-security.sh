4f07fcad17157f7e7c7208ac5b7d6c6080718f8e
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2015-05-30 01:04:33 +0200

    restrictive PATH
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- docker-bench-security.sh
+++ docker-bench-security.sh
@@ -19,6 +19,7 @@ dir_name=`dirname ${this_path}`    ## Dir where this file is
 myname=`basename ${this_path}`     ## file name of this script.
 logger="${myname}.log"
 
+export PATH=/bin:/sbin:/usr/bin
 
 # Check for required program(s)
 req_progs='docker netstat grep awk'
