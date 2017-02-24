7c66b6373a39d96b4eb5a17a861f74ac3ae067ed
Andreas Stieger <astieger@suse.com>
2017-02-24 10:17:33 +0100

    2.17: grep -e recognizes IPv4 separator any character - escape
    Fixes #224 - no. 2
    
    Signed-off-by: Andreas Stieger <astieger@suse.com>

--- tests/2_docker_daemon_configuration.sh
+++ tests/2_docker_daemon_configuration.sh
@@ -161,7 +161,7 @@ fi
 # 2.17
 check_2_17="2.17 - Bind swarm services to a specific host interface"
 if docker info 2>/dev/null | grep -e "Swarm:*\sactive\s*" >/dev/null 2>&1; then
-  netstat -lnt | grep -e '\[::]:2377' -e '*:2377' -e '0.0.0.0:2377' >/dev/null 2>&1
+  netstat -lnt | grep -e '\[::]:2377' -e '*:2377' -e '0\.0\.0\.0:2377' >/dev/null 2>&1
   if [ $? -eq 1 ]; then
     pass "$check_2_17"
   else
