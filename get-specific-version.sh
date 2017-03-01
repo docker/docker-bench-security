c873f24e56b3425d35ef6cd4cef684b3e7d1f4a7
Julien Del-Piccolo <julien@del-piccolo.com>
2017-03-01 14:10:25 +0100

    Fix...
    Signed-off-by: Julien Del-Piccolo <julien@del-piccolo.com>

--- get-specific-version.sh
+++ get-specific-version.sh
@@ -5,16 +5,16 @@ if [ -n "${DBS_VERSION}" ]; then
     wget -q -P /usr/share/docker-bench-security/ https://github.com/docker/docker-bench-security/archive/${DBS_VERSION}.tar.gz
     rm -rf /usr/share/docker-bench-security/${DBS_VERSION}/
     mkdir -p /usr/share/docker-bench-security/${DBS_VERSION}/
-    tar xfz /usr/share/docker-bench-security/${DBS_VERSION}.tar.gz -C /usr/share/docker-bench-security/${DBS_VERSION} --strip 1 --overwrite
+    tar xfzv /usr/share/docker-bench-security/${DBS_VERSION}.tar.gz -C /usr/share/docker-bench-security/${DBS_VERSION} --strip 1 --overwrite
     rm -rfv /usr/share/docker-bench-security/${DBS_VERSION}.tar.gz
   fi
 
-  if [ $(find /usr/share/docker-bench-security/ | wc -l) -gt 1  ]; then
-    rm -rf /usr/local/bin/docker-bench-security.sh /usr/local/bin/helper_lib.sh /usr/local/bin/output_lib.sh
-    rm -rf /usr/local/bin/tests/*
+  if [ $(find /usr/share/docker-bench-security/${DBS_VERSION}/ | wc -l) -gt 1  ]; then
+    rm -rfv /usr/local/bin/docker-bench-security.sh /usr/local/bin/helper_lib.sh /usr/local/bin/output_lib.sh
+    rm -rfv /usr/local/bin/tests/*
 
-    cp -r /usr/share/docker-bench-security/${DBS_VERSION}/*.sh /usr/local/bin/
-    cp -r /usr/share/docker-bench-security/${DBS_VERSION}/tests/*.sh /usr/local/bin/tests/
+    cp -rv /usr/share/docker-bench-security/${DBS_VERSION}/*.sh /usr/local/bin/
+    cp -rv /usr/share/docker-bench-security/${DBS_VERSION}/tests/*.sh /usr/local/bin/tests/
   fi
 fi
 
