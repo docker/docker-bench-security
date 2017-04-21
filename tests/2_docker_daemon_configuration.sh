5d9101cfc2ba814450d4292a500f3bec0637c77f
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-04-21 13:51:09 +0200

    .Server.Experimental pre-1.13
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- tests/2_docker_daemon_configuration.sh
+++ tests/2_docker_daemon_configuration.sh
@@ -253,7 +253,7 @@ fi
 
 # 2.21
 check_2_21="2.21 - Avoid experimental features in production"
-if docker info 2>/dev/null | grep -e "Experimental:\s*false*" 2>/dev/null 1>&2; then
+if docker version -f '{{.Server.Experimental}}' | grep false 2>/dev/null 1>&2; then
   pass "$check_2_21"
 else
   warn "$check_2_21"
