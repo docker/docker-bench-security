2aa9719dd653d6575644ae3bcdace77536f73875
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2018-01-18 11:29:20 +0100

    silence example check output
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- tests/99_community_checks.sh
+++ tests/99_community_checks.sh
@@ -8,7 +8,7 @@ check_c() {
 check_c_1() {
   check_c_1="C.1  - This is a example check"
   totalChecks=$((totalChecks + 1))
-  if docker info --format='{{ .Architecture }}' | grep 'x86_64'; then
+  if docker info --format='{{ .Architecture }}' | grep 'x86_64' 2>/dev/null 1>&2; then
     pass "$check_c_1"
     logjson "c.1" "PASS"
   else
