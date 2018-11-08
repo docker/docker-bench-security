37ccf4dbcf740998cc1ff51d7b3f3b81690612e6
Cheng-Li Jerry Ma <chengli.ma@gmail.com>
2018-11-08 15:05:28 -0700

    Fix -e option last entry is not excluded in docker
    Signed-off-by: Cheng-Li Jerry Ma <chengli.ma@gmail.com>

--- docker-bench-security.sh
+++ docker-bench-security.sh
@@ -126,7 +126,7 @@ main () {
     cis
   elif [ -z "$check" ] && [ "$checkexclude" ]; then
     checkexcluded="$(echo ",$checkexclude" | sed -e 's/^/\^/g' -e 's/,/\$|/g' -e 's/$/\$/g')"
-    grep 'check_[0-9]' functions_lib.sh | grep -vE "\'$checkexcluded\'" | while read -r c; do
+    grep 'check_[0-9]' functions_lib.sh | grep -vE "$checkexcluded" | while read -r c; do
       "$c"
     done
   else
