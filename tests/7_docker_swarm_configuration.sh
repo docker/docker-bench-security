8aec461d46a794c2b6022502841dcc57f5d7ec92
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2020-05-08 13:09:52 +0200

    more flexible binary usage, better support for mac os
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- tests/7_docker_swarm_configuration.sh
+++ tests/7_docker_swarm_configuration.sh
@@ -63,7 +63,7 @@ check_7_3() {
 
   totalChecks=$((totalChecks + 1))
   if docker info 2>/dev/null | grep -e "Swarm:*\sactive\s*" >/dev/null 2>&1; then
-    ss -lnt | grep -e '\[::]:2377 ' -e ':::2377' -e '*:2377 ' -e ' 0\.0\.0\.0:2377 ' >/dev/null 2>&1
+    $netbin -lnt | grep -e '\[::]:2377 ' -e ':::2377' -e '*:2377 ' -e ' 0\.0\.0\.0:2377 ' >/dev/null 2>&1
     if [ $? -eq 1 ]; then
       pass "$check_7_3"
       resulttestjson "PASS"
