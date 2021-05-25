2466ab4826448a8d9158588bd0a6711047336789
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-05-25 20:45:38 +0200

    update wording
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- functions/helper_lib.sh
+++ functions/helper_lib.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Returns the absolute path of a given string
 abspath () { case "$1" in /*)printf "%s\n" "$1";; *)printf "%s\n" "$PWD/$1";; esac; }
@@ -145,6 +145,6 @@ yell "# ------------------------------------------------------------------------
 # Docker, Inc. (c) 2015-$(date +"%Y")
 #
 # Checks for dozens of common best-practices around deploying Docker containers in production.
-# Inspired by the CIS Docker Benchmark v1.2.0.
+# Based on the CIS Docker Benchmark 1.3.1.
 # --------------------------------------------------------------------------------------------"
 }
