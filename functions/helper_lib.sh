e081393ad752277958e9f7aedb3287cdc30502a1
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2024-04-11 21:45:15 +0000

    include /run in get_service_file
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- functions/helper_lib.sh
+++ functions/helper_lib.sh
@@ -144,6 +144,10 @@ get_service_file() {
     systemctl show -p FragmentPath "$SERVICE" | sed 's/.*=//'
     return
   fi
+  if find /run -name "$SERVICE" 2> /dev/null 1>&2; then
+    find /run -name "$SERVICE" | head -n1
+    return
+  fi
   echo "/usr/lib/systemd/system/$SERVICE"
 }
 
