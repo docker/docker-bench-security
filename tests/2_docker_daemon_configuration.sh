200ffe8926437d16d836c54769f239c946193413
Arcélone <62914065+Arcelone@users.noreply.github.com>
2025-09-03 22:10:45 +0200

    Add new test for section 2.7 It also update the ID of other test of section 2

--- tests/2_docker_daemon_configuration.sh
+++ tests/2_docker_daemon_configuration.sh
@@ -128,7 +128,7 @@ check_2_6() {
   local id="2.6"
   local desc="Ensure aufs storage driver is not used (Scored)"
   local remediation="Do not start Docker daemon as using dockerd --storage-driver aufs option."
-  local remediationImpact="aufs is the only storage driver that allows containers to share executable and shared  library memory. Its use should be reviewed in line with your organization's security policy."
+  local remediationImpact="aufs is the only storage driver that allows containers to share executable and shared library memory. Its use should be reviewed in line with your organization's security policy."
   local check="$id - $desc"
   starttestjson "$id" "$desc"
 
@@ -143,13 +143,30 @@ check_2_6() {
 
 check_2_7() {
   local id="2.7"
+  local desc="Ensure devicemapper storage driver is not used (Scored)"
+  local remediation="Do not start Docker daemon as using dockerd --storage-driver devicemapper option."
+  local remediationImpact="If you are using Device Mapper, you must migrate to a supported storage driver before upgrading to Docker Engine v25.0., it has been removed in Docker Engine v25.0"
+  local check="$id - $desc"
+  starttestjson "$id" "$desc"
+
+  if docker info 2>/dev/null | grep -e "^\sStorage Driver:\s*devicemapper\s*$" >/dev/null 2>&1; then
+    warn -s "$check"
+    logcheckresult "WARN"
+    return
+  fi
+  pass -s "$check"
+  logcheckresult "PASS"
+}
+
+check_2_8() {
+  local id="2.8"
   local desc="Ensure TLS authentication for Docker daemon is configured (Scored)"
   local remediation="Follow the steps mentioned in the Docker documentation or other references. By default, TLS authentication is not configured."
   local remediationImpact="You would need to manage and guard certificates and keys for the Docker daemon and Docker clients."
   local check="$id - $desc"
   starttestjson "$id" "$desc"
 
-  if $(grep -qE "host.*tcp://" "$CONFIG_FILE") || \
+  if $(grep -qE "host.*tcp://" "$CONFIG_FILE") || \
     [ $(get_docker_cumulative_command_line_args '-H' | grep -vE '(unix|fd)://') > /dev/null 2>&1 ]; then
     if [ $(get_docker_configuration_file_args '"tlsverify":' | grep 'true') ] || \
         [ $(get_docker_cumulative_command_line_args '--tlsverify' | grep 'tlsverify') >/dev/null 2>&1 ]; then
@@ -174,8 +191,8 @@ check_2_7() {
   logcheckresult "INFO" "Docker daemon not listening on TCP"
 }
 
-check_2_8() {
-  local id="2.8"
+check_2_9() {
+  local id="2.9"
   local desc="Ensure the default ulimit is configured appropriately (Manual)"
   local remediation="Run Docker in daemon mode and pass --default-ulimit as option with respective ulimits as appropriate in your environment and in line with your security policy. Example: dockerd --default-ulimit nproc=1024:2048 --default-ulimit nofile=100:200"
   local remediationImpact="If ulimits are set incorrectly this could cause issues with system resources, possibly causing a denial of service condition."
@@ -197,8 +214,8 @@ check_2_8() {
   logcheckresult "INFO" "Default ulimit doesn't appear to be set"
 }
 
-check_2_9() {
-  local id="2.9"
+check_2_10() {
+  local id="2.10"
   local desc="Enable user namespace support (Scored)"
   local remediation="Please consult the Docker documentation for various ways in which this can be configured depending upon your requirements. The high-level steps are: Ensure that the files /etc/subuid and /etc/subgid exist. Start the docker daemon with --userns-remap flag."
   local remediationImpact="User namespace remapping is incompatible with a number of Docker features and also currently breaks some of its functionalities."
@@ -219,8 +236,8 @@ check_2_9() {
   logcheckresult "WARN"
 }
 
-check_2_10() {
-  local id="2.10"
+check_2_11() {
+  local id="2.11"
   local desc="Ensure the default cgroup usage has been confirmed (Scored)"
   local remediation="The default setting is in line with good security practice and can be left in situ."
   local remediationImpact="None."
@@ -243,8 +260,8 @@ check_2_10() {
   logcheckresult "PASS"
 }
 
-check_2_11() {
-  local id="2.11"
+check_2_12() {
+  local id="2.12"
   local desc="Ensure base device size is not changed until needed (Scored)"
   local remediation="Do not set --storage-opt dm.basesize until needed."
   local remediationImpact="None."
@@ -265,8 +282,8 @@ check_2_11() {
   logcheckresult "PASS"
 }
 
-check_2_12() {
-  local id="2.12"
+check_2_13() {
+  local id="2.13"
   local desc="Ensure that authorization for Docker client commands is enabled (Scored)"
   local remediation="Install/Create an authorization plugin. Configure the authorization policy as desired. Start the docker daemon using command dockerd --authorization-plugin=<PLUGIN_ID>"
   local remediationImpact="Each Docker command needs to pass through the authorization plugin mechanism. This may have a performance impact"
@@ -287,8 +304,8 @@ check_2_12() {
   logcheckresult "WARN"
 }
 
-check_2_13() {
-  local id="2.13"
+check_2_14() {
+  local id="2.14"
   local desc="Ensure centralized and remote logging is configured (Scored)"
   local remediation="Set up the desired log driver following its documentation. Start the docker daemon using that logging driver. Example: dockerd --log-driver=syslog --log-opt syslog-address=tcp://192.xxx.xxx.xxx"
   local remediationImpact="None."
@@ -304,8 +321,8 @@ check_2_13() {
   logcheckresult "PASS"
 }
 
-check_2_14() {
-  local id="2.14"
+check_2_15() {
+  local id="2.15"
   local desc="Ensure containers are restricted from acquiring new privileges (Scored)"
   local remediation="You should run the Docker daemon using command: dockerd --no-new-privileges"
   local remediationImpact="no_new_priv prevents LSMs such as SELinux from escalating the privileges of individual containers."
@@ -326,8 +343,8 @@ check_2_14() {
   logcheckresult "WARN"
 }
 
-check_2_15() {
-  local id="2.15"
+check_2_16() {
+  local id="2.16"
   local desc="Ensure live restore is enabled (Scored)"
   local remediation="Run Docker in daemon mode and pass --live-restore option."
   local remediationImpact="None."
@@ -353,8 +370,8 @@ check_2_15() {
   logcheckresult "WARN"
 }
 
-check_2_16() {
-  local id="2.16"
+check_2_17() {
+  local id="2.17"
   local desc="Ensure Userland Proxy is Disabled (Scored)"
   local remediation="You should run the Docker daemon using command: dockerd --userland-proxy=false"
   local remediationImpact="Some systems with older Linux kernels may not be able to support hairpin NAT and therefore require the userland proxy service. Also, some networking setups can be impacted by the removal of the userland proxy."
@@ -375,8 +392,8 @@ check_2_16() {
   logcheckresult "WARN"
 }
 
-check_2_17() {
-  local id="2.17"
+check_2_18() {
+  local id="2.18"
   local desc="Ensure that a daemon-wide custom seccomp profile is applied if appropriate (Manual)"
   local remediation="By default, Docker's default seccomp profile is applied. If this is adequate for your environment, no action is necessary."
   local remediationImpact="A misconfigured seccomp profile could possibly interrupt your container environment. You should therefore exercise extreme care if you choose to override the default settings."
@@ -392,11 +409,11 @@ check_2_17() {
   logcheckresult "INFO"
 }
 
-check_2_18() {
+check_2_19() {
   docker_version=$(docker version | grep -i -A2 '^server' | grep ' Version:' \
     | awk '{print $NF; exit}' | tr -d '[:alpha:]-,.' | cut -c 1-4)
 
-  local id="2.18"
+  local id="2.19"
   local desc="Ensure that experimental features are not implemented in production (Scored)"
   local remediation="You should not pass --experimental as a runtime parameter to the Docker daemon on production systems."
   local remediationImpact="None."
