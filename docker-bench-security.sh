0d7b29eb00beabd42e196ef4d0c760f765457edb
Arcélone <62914065+Arcelone@users.noreply.github.com>
2025-09-04 00:29:13 +0200

    move to bash in help message

--- docker-bench-security.sh
+++ docker-bench-security.sh
@@ -44,11 +44,11 @@ Usage: ${myname}.sh [OPTIONS]
 
 Example:
   - Only run check "2.2 - Ensure the logging level is set to 'info'":
-      sh docker-bench-security.sh -c check_2_2
+      bash docker-bench-security.sh -c check_2_2
   - Run all available checks except the host_configuration group and "2.8 - Enable user namespace support":
-      sh docker-bench-security.sh -e host_configuration,check_2_8
+      bash docker-bench-security.sh -e host_configuration,check_2_8
   - Run just the container_images checks except "4.5 - Ensure Content trust for Docker is Enabled":
-      sh docker-bench-security.sh -c container_images -e check_4_5
+      bash docker-bench-security.sh -c container_images -e check_4_5
 
 Options:
   -b           optional  Do not print colors
